d5bf7944e14feddc97fff56950520516ac6b1347
Maya Dong <127631422+mayamengya@users.noreply.github.com>
2024-04-07 00:35:34 -0700

    inital commit of frontend-2

---
+++ frontend-2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Chatbot from '/Users/Lemon/Desktop/Coding/YouCode-2024/frontend-2/src/components/chatbot/chatbot.js'; 
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Chatbot />
+      </header>
+    </div>
+  );
+}
+
+export default App;
